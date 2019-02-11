f8aa8512b85030e0ebaf7713e33ae433ddb47f85
Salvatore Laisa <moebiusmania@gmail.com>
2019-02-11 11:23:27 +0100

    updated component generator

--- scripts/templates/story.ts
+++ scripts/templates/story.ts
@@ -1,20 +1,13 @@
 /* global document */
 import { storiesOf, addDecorator } from '@storybook/polymer';
 import { withKnobs, text } from '@storybook/addon-knobs';
-import { withNotes } from '@storybook/addon-notes';
 // import { action } from '@storybook/addon-actions';
 import { html, TemplateResult } from 'lit-html';
-import markdown from './../docs/%%%.md';
 import '../../src/components/%%%';
 
-addDecorator(withNotes);
 addDecorator(withKnobs);
 
 storiesOf('%%%', module)
   .add('Playground', (): TemplateResult => html`<amber-%%%
   
-></amber-%%%>`, {
-      notes: {
-        markdown
-      }
-    });
+></amber-%%%>`);
