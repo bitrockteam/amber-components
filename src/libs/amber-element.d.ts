fc28fadb4f18afa77f727372344d0d54d9464187
Salvatore Laisa <moebiusmania@gmail.com>
2019-05-16 11:59:17 +0200

    more unit tests

--- src/libs/amber-element.d.ts
+++
@@ -1,6 +0,0 @@
-import { LitElement } from 'lit-element';
-import { customElement } from './decorators';
-declare class AmberElement extends LitElement {
-    triggerEvent(name: string, detail?: object): boolean;
-}
-export { AmberElement, customElement, };
